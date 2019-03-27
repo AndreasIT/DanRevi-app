ab4f4b72214abe3c60462890e6b51d3f545ab1de
AndreasIT <32296603+AndreasIT@users.noreply.github.com>
2019-03-27 10:26:02 +0100

    Add project files.

---
+++ DanRevi/DanRevi/CalendarContentPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XamForms.Controls;
+using Xamarin.Forms;
+
+namespace DanRevi
+{
+    public class CalendarContentPage : ContentPage
+    {
+
+        public CalendarContentPage()
+        {
+            //this.Padding = new Thickness(10, 10, 10, 10);
+            StackLayout panel = new StackLayout();
+            // panel.Spacing = 10;
+
+            //panel.Children.Add(new TableView {  });
+            // panel.Children.Add(new Label { Text = "hi" });
+            var calendar = new Calendar();
+
+            calendar.DateClicked += Calendar_DateClicked;
+
+            calendar.BorderColor = Color.Gray;
+            calendar.BorderWidth = 3;
+            calendar.BackgroundColor = Color.PaleGoldenrod;
+            calendar.StartDay = DayOfWeek.Sunday;
+            calendar.StartDate = DateTime.Now;
+            calendar.WeekdaysTextColor = Color.Black;
+
+            calendar.SpecialDates = new List<SpecialDate>
+            {
+                new SpecialDate(DateTime.Now) { BackgroundColor = Color.Green, BorderColor = Color.Black, BorderWidth=4, Selectable = true }
+            };
+
+            panel.Children.Add(calendar);
+
+            this.Content = panel;
+        }
+
+        private async void Calendar_DateClicked(object sender, DateTimeEventArgs e)
+        {
+            
+            await Navigation.PushAsync(new SelectedDate(e));
+        }
+    }
+}
---
+++ DanRevi/DanRevi/SelectedDate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XamForms.Controls;
+using Xamarin.Forms;
+
+namespace DanRevi
+{
+	public class SelectedDate : ContentPage
+	{
+        Label centerLabel;
+        BoxView outerBox;
+
+		public SelectedDate (DateTimeEventArgs e)
+		{
+            AbsoluteLayout outerLayout = new AbsoluteLayout();
+            RelativeLayout layout = new RelativeLayout();
+
+            //content = new stacklayout {
+            //	children = { new label {
+            //                 text = "you have arrived at your selected date: " + e.datetime.tostring("dd-mm-yy") + "!" }
+            //	}
+            //};
+
+            outerBox = new BoxView { Color = Color.LightGray, HeightRequest = 8, WidthRequest = 5 };
+            centerLabel = new Label { Text = "Hi" };
+
+            layout.Children.Add(outerBox, Constraint.RelativeToParent((parent) => {
+                return parent.X + 85;
+            }), Constraint.RelativeToParent((parent) => {
+                return parent.Y + 20;
+            }), Constraint.RelativeToParent((parent) => {
+                return (parent.Width * .3);
+            }), Constraint.RelativeToParent((parent) => {
+                return (parent.Height * .3);
+            }));
+            layout.Children.Add(centerLabel, Constraint.RelativeToParent((parent) => {
+                return parent.X + 85;
+            }), Constraint.RelativeToParent((parent) => {
+                return parent.Y + 20;
+            }), Constraint.RelativeToParent((parent) => {
+                return (parent.Width * .5) - 50;
+            }), Constraint.RelativeToParent((parent) => {
+                return (parent.Height * .5) - 50;
+            }));
+
+            outerLayout.Children.Add(layout, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.All);
+
+            this.Content = outerLayout;
+        }
+	}
+}
