b9e79c4a3fb6320de12f2ba7c6a93542868834ab
AndreasIT <32296603+AndreasIT@users.noreply.github.com>
2019-05-01 15:10:18 +0200

    Gjort MainOage.Xaml.cs mere overskuelig. Tilføjet kurser til MainPage. Ændret Datetime i Xaml. tilføjet headers.

---
+++ DanRevi/DanRevi/Courses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DanRevi
+{
+    public class Courses : INotifyPropertyChanged
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public string target_audience { get; set; }
+
+        public string location { get; set; }
+
+        public DateTime start { get; set; }
+
+        public DateTime end { get; set; }
+
+        public string host { get; set; }
+
+        public string startEndDate { get { return "Start: " + start + " Slut: " + end; } }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- DanRevi/DanRevi/MainPage.xaml.cs
+++ DanRevi/DanRevi/MainPage.xaml.cs
@@ -12,9 +12,13 @@ namespace DanRevi
 {
     public partial class MainPage : ContentPage
     {
-        private const string Url = "http://danrevi-api.azurewebsites.net/api/deadlines"; //The link for Daniels API
         private readonly HttpClient _client = new HttpClient(); //Creating a new instance of HttpClient. (Microsoft.Net.Http)
+        //Deadlines
+        private const string UrlDL = "http://danrevi-api.azurewebsites.net/api/deadlines"; //The link for Daniels API
         private ObservableCollection<Deadlines> _deadlines; //Refreshing the state of the UI in realtime when updating the ListView's Collection
+        //Courses
+        private const string UrlC = "http://danrevi-api.azurewebsites.net/api/courses"; //The link for Daniels API
+        private ObservableCollection<Courses> _courses; //Refreshing the state of the UI in realtime when updating the ListView's Collection
 
         /// <summary>
         /// Constructor
@@ -30,11 +34,27 @@ namespace DanRevi
         /// </summary>
         protected override async void OnAppearing()
         {
-            string content = await _client.GetStringAsync(Url); //Sends a GET request to the specified Uri and returns the response body as a string in an asynchronous operation
+              await GetDeadlines();
+              await GetCourses();
+              base.OnAppearing();
+        }
+
+        private async Task GetDeadlines()
+        {
+            string content = await _client.GetStringAsync(UrlDL); //Sends a GET request to the specified Uri and returns the response body as a string in an asynchronous operation
             List<Deadlines> deadlines = JsonConvert.DeserializeObject<List<Deadlines>>(content); //Deserializes or converts JSON String into a collection of Deadlines
             _deadlines = new ObservableCollection<Deadlines>(deadlines); //Converting the List to ObservalbleCollection of Deadlines
             DeadlinesListView.ItemsSource = _deadlines; //Assigning the ObservableCollection to DeadlinesListView in the XAML of this file
-            base.OnAppearing();
+
+        }
+
+        private async Task GetCourses()
+        {
+            string content = await _client.GetStringAsync(UrlC); //Sends a GET request to the specified Uri and returns the response body as a string in an asynchronous operation
+            List<Courses> courses = JsonConvert.DeserializeObject<List<Courses>>(content); //Deserializes or converts JSON String into a collection of Courses
+            _courses = new ObservableCollection<Courses>(courses); //Converting the List to ObservalbleCollection of Courses
+            CoursesListView.ItemsSource = _courses; //Assigning the ObservableCollection to CoursesListView in the XAML of this file
+
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -43,21 +63,21 @@ namespace DanRevi
         }
     }
 
-    public class DataClassMultiValueConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (values[0] is Deadlines)
-            {
-                Deadlines data = values[0] as Deadlines;
-                return String.Format("{0} {1}", data.name, data.description);
-            }
-            else { return ""; }
-        }
-    }
+    //public class DataClassMultiValueConverter : IMultiValueConverter
+    //{
+    //    public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    //    {
+    //        if (values[0] is Deadlines)
+    //        {
+    //            Deadlines data = values[0] as Deadlines;
+    //            return String.Format("{0} {1}", data.name, data.description);
+    //        }
+    //        else { return ""; }
+    //    }
+    //}
 
-    public interface IMultiValueConverter
-    {
-        object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture);
-    }
+    //public interface IMultiValueConverter
+    //{
+    //    object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture);
+    //}
 }
