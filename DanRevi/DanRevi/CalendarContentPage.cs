926f8f7af31d9dd10b76c4ccbbd39a2d9e43dce4
AndreasIT <32296603+AndreasIT@users.noreply.github.com>
2019-05-03 14:19:08 +0200

    Deadlines bliver nu vist på kalenderens forside, på de dage der er en deadline.

--- DanRevi/DanRevi/CalendarContentPage.cs
+++ DanRevi/DanRevi/CalendarContentPage.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using XamForms.Controls;
 using Xamarin.Forms;
+using System.Net.Http;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace DanRevi
 {
     public class CalendarContentPage : ContentPage
     {
+        private readonly HttpClient _client = new HttpClient(); //Creating a new instance of HttpClient. (Microsoft.Net.Http)
+        //Deadlines
+        private const string UrlDL = "http://danrevi-api.azurewebsites.net/api/deadlines"; //The link for Daniels API
 
-        public CalendarContentPage()
+        protected override async void OnAppearing()
         {
+
             //this.Padding = new Thickness(10, 10, 10, 10);
             StackLayout panel = new StackLayout();
             // panel.Spacing = 10;
@@ -29,14 +37,29 @@ namespace DanRevi
             calendar.StartDate = DateTime.Now;
             calendar.WeekdaysTextColor = Color.Black;
 
-            calendar.SpecialDates = new List<SpecialDate>
-            {
-                new SpecialDate(DateTime.Now) { BackgroundColor = Color.Green, BorderColor = Color.Black, BorderWidth=4, Selectable = true }
-            };
+            var deadlines = await GetDeadlines();
+
+            calendar.SpecialDates = deadlines.Select(d => new SpecialDate(d.date) { BackgroundColor = Color.Blue, Selectable = true }).ToList();
+            calendar.SpecialDates.Add(new SpecialDate(DateTime.Now) { BackgroundColor = Color.Green, BorderColor = Color.Black, BorderWidth = 4, Selectable = true });
 
             panel.Children.Add(calendar);
 
             this.Content = panel;
+
+            base.OnAppearing();
+        }
+
+        //public CalendarContentPage()
+        //{
+            
+        //}
+
+        private async Task<ObservableCollection<Deadlines>> GetDeadlines()
+        {
+            string content = await _client.GetStringAsync(UrlDL); //Sends a GET request to the specified Uri and returns the response body as a string in an asynchronous operation
+            List<Deadlines> deadlines = JsonConvert.DeserializeObject<List<Deadlines>>(content); //Deserializes or converts JSON String into a collection of Deadlines
+            return new ObservableCollection<Deadlines>(deadlines); //Converting the List to ObservalbleCollection of Deadlines
+
         }
 
         private async void Calendar_DateClicked(object sender, DateTimeEventArgs e)
